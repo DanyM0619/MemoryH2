e3c3d5ef26146004513117d84f6f1584f4dd938f
DanyM0619 <daedmegu@gmaiil.com>
2024-02-16 13:26:00 -0500

    Se realiza la persistencia con memoria H2

---
+++ src/main/java/com/example/Memory/MemoryH2Application.java
@@ -0,0 +1,13 @@
+package com.example.Memory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoryH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoryH2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Memory/MemoryH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Memory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoryH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
